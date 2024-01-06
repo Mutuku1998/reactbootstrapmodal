eab2f4052a9f2a2eb624b0058118abeffdff2faf
mutuku1998 <martinmutuku1998@gmail.com>
2024-01-06 07:30:04 +0300

    responsive nav

---
+++ reactmodald/src/Components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+const Header = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Modal example</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About Us</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Services</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+              Web development
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Software development</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+               System design
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#link">Contact Us</Nav.Link>
+            <Nav.Link href="#link">Gallery</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  
+  );
+}
+
+export default Header
---
+++ reactmodald/src/Components/Modald.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Modald = () => {
+  return (
+    <div>Modald</div>
+  )
+}
+
+export default Modald
